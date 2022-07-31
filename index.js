2a996a1eae82c873fd9e6786768f5a1f41ad5f12
Rudy Lemus <rudolfolemus@gmail.com>
2022-07-31 16:34:32 -0700

    heroku changes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const express = require('express'),
   Models = require('./models.js'),
   Movies = Models.Movie,
   Users = Models.User;
+const { check, validationResult } = require('express-validator');
 
 mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -15,6 +16,9 @@ app.use(bodyParser.json());
 app.use(express.static('public')); 
 
 app.use(morgan('common'));
+
+const cors = require('cors');
+app.use(cors());
 
 let auth = require('./auth')(app);
 
@@ -27,7 +31,18 @@ app.get('/', (req, res) => {
   });  
 
 // Add new user
-app.post('/users', (req, res) => {
+app.post('/users', [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username contains no alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required'.).not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail() 
+], (req, res) => {
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    let hashedPassword = Users.hashPassword
+    (req.body.Password);
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
@@ -35,9 +50,8 @@ app.post('/users', (req, res) => {
       } else {
         Users
           .create({
-            Name: req.body.Name,
             Username: req.body.Username,
-            Password: req.body.Password,
+            Password: hashedPassword,
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })
@@ -215,6 +229,7 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something went wrong!');
 });
 
-app.listen(8080, () => {
-  console.log('This app is running on port 8080');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
