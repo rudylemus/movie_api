50501371875a1ae5c80872cf2e032a034c3dd30e
Rudy Lemus <rudolfolemus@gmail.com>
2022-08-04 21:28:43 -0700

    updated connection URI

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const express = require('express'),
   Users = Models.User;
 const { check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(bodyParser.json());
 
